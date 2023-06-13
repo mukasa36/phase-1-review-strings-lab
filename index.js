5c1067ec35fcd04a7890ed4e45f2e26b6029231d
emmanuel <emmanuel.mukasa@moringaschool.com>
2023-06-13 13:58:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = 'Welcome,  ${currentUser[G].slice(0, 1).currentUser.slice(0, 2)}!';
